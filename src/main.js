7562a73dae7fe8cb3f449a7605af4fe673be17be
Memeda124 <876508489@qq.com>
2019-11-12 21:47:20 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import './styles/index.less'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
