614d57737938fd95321d91c620a5dec079e755bd
Memeda124 <876508489@qq.com>
2019-11-13 18:11:01 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import './styles/index.less'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
-Vue.config.productionTip = false
-import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.config.productionTip = false // 引入样式
 
 new Vue({
   router,
