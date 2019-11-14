b88c7a828f6744dc0fc7ec56dd16aa1c8791c2bd
Memeda124 <876508489@qq.com>
2019-11-14 22:33:13 +0800

    黑马头条PC-主页模块-页面布局

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import Element from 'element-ui'
 import './styles/index.less'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios // 将axios共享给所有的实例使用
-Vue.use(ElementUI)
+Vue.use(Element) // 注册整个的所有的elementUL组件 Vue.use调用了elementUI里面的一个方法 install
+
 Vue.config.productionTip = false // 引入样式
 
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    主页
-  </div>
+  <!-- 布局 -->
+  <el-container>
+    <!-- 左右布局 -->
+    <el-aside>左</el-aside>
+    <!-- 大容器 -->
+    <el-container>
+      <el-header>上</el-header>
+      <el-main>下</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
