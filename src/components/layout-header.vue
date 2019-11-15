be1b4771816263d2c77104ef2858dd774f824eda
Memeda124 <876508489@qq.com>
2019-11-15 17:47:17 +0800

    黑马头条PC-主页模块-左侧导航菜单及头部结构-下拉菜单

---
+++ src/components/layout-header.vue
@@ -0,0 +1,52 @@
+<template>
+<!-- 头部组件 el-row布局  -->
+<el-row type = 'flex' justify="space-between" align='middle'>
+    <!-- 左侧内容 -->
+    <el-col :span='6' class="left">
+        <i class='el-icon-s-fold'></i>
+        <span>江苏传智博客教育科技股份有限公司</span>
+    </el-col>
+    <!-- 右侧内容 -->
+    <el-col :span='3' class="right">
+        <!-- 头像 -->
+        <img src="../assets/img/avatar.jpg" alt="">
+        <!-- 下拉菜单 -->
+            <el-dropdown trigger='click'>
+            <span>噜噜噜</span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>账户信息</el-dropdown-item>
+                <el-dropdown-item>git地址</el-dropdown-item>
+                <el-dropdown-item>退出</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.left {
+    display: flex;
+    align-items: center;
+    i {
+        font-size: 24px;
+    }
+    span {
+        margin-left: 4px;
+    }
+}
+.right {
+    display: flex;
+    align-items: center;
+    img {
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+}
+
+</style>
